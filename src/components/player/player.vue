f4d916149a60d19428ba8b3a0c6bbb16902a8fd8
pangfeng <pangjianfeng@huanai.com>
2021-05-15 10:06:53 +0800

    歌曲暂停与播放

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -13,8 +13,29 @@
         <h1 class="title">{{ currentSong.name }}</h1>
         <h2 class="subtitle">{{ currentSong.singer }}</h2>
       </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div @click="togglePlay" class="icon i-center">
+            <i :class="playIcon"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <audio ref="audioRef"></audio>
+    <audio
+        ref="audioRef"
+        @pause="pause"></audio>
   </div>
 </template>
 
@@ -30,27 +51,46 @@ export default {
     const store = useStore()
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
+    const playing = computed(() => store.state.playing)
+
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
 
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return
       }
       const audioEl = audioRef.value
-      console.log(audioEl)
-      console.log(audioEl.play)
       audioEl.src = newSong.url
       audioEl.play()
+    })
+
+    watch(playing, (newPlaying) => {
+      const audioEl = audioRef.value
+      newPlaying ? audioEl.play() : audioEl.pause()
     })
 
     function goBack () {
       store.commit('setFullScreen', false)
     }
 
+    function togglePlay () {
+      store.commit('setPlayingState', !playing.value)
+    }
+
+    function pause () {
+      store.commit('setPlayingState', false)
+    }
+
     return {
-      goBack,
       audioRef,
       fullScreen,
-      currentSong
+      currentSong,
+      playIcon,
+      goBack,
+      togglePlay,
+      pause
     }
   }
 }
