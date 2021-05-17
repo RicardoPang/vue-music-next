82a1e4fd395c9f8a32273ddc25b83c69d6b57af2
pangfeng <pangjianfeng@huanai.com>
2021-05-17 17:38:48 +0800

    搜索页面搜索框开发

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>Search</div>
+  <div class="search">
+    <div class="search-input-wrapper">
+      <search-input v-model="query"></search-input>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchInput from '@/components/search/search-input'
+import { ref, watch } from 'vue'
+
 export default {
-  name: 'search'
+  name: 'search',
+  components: { SearchInput },
+  setup () {
+    const query = ref('')
+
+    watch(query, (val) => {
+      console.log(val)
+    })
+
+    return {
+      query
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.search {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
 
+  .search-input-wrapper {
+    margin: 20px;
+  }
+}
 </style>
