c4184ad8408d491610b33cce1bee121b095b51fc
pangfeng <pangjianfeng@huanai.com>
2021-05-17 17:53:49 +0800

    搜索页面热门搜索开发

--- src/views/search.vue
+++ src/views/search.vue
@@ -3,25 +3,47 @@
     <div class="search-input-wrapper">
       <search-input v-model="query"></search-input>
     </div>
+    <div class="search-content">
+      <div class="hot-keys">
+        <h1 class="title">热门搜索</h1>
+        <ul>
+          <li
+              class="item"
+              v-for="item in hotKeys"
+              :key="item.id"
+              @click="addQuery(item.key)">
+            <span>{{ item.key }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import SearchInput from '@/components/search/search-input'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
+import { getHotKeys } from '@/service/search'
 
 export default {
   name: 'search',
   components: { SearchInput },
   setup () {
     const query = ref('')
+    const hotKeys = ref([])
 
-    watch(query, (val) => {
-      console.log(val)
+    getHotKeys().then((result) => {
+      hotKeys.value = result.hotKeys
     })
 
+    function addQuery (s) {
+      query.value = s
+    }
+
     return {
-      query
+      query,
+      hotKeys,
+      addQuery
     }
   }
 }
@@ -38,6 +60,62 @@ export default {
 
   .search-input-wrapper {
     margin: 20px;
+  }
+
+  .search-content {
+    flex: 1;
+    overflow: hidden;
+
+    .hot-keys {
+      margin: 0 20px 20px 20px;
+
+      .title {
+        margin-bottom: 20px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+      }
+
+      .item {
+        display: inline-block;
+        padding: 5px 10px;
+        margin: 0 20px 10px 0;
+        border-radius: 6px;
+        background: $color-highlight-background;
+        font-size: $font-size-medium;
+        color: $color-text-d;
+      }
+    }
+
+    .search-history {
+      position: relative;
+      margin: 0 20px;
+
+      .title {
+        display: flex;
+        align-items: center;
+        height: 40px;
+        font-size: $font-size-medium;
+        color: $color-text-l;
+
+        .text {
+          flex: 1;
+        }
+
+        .clear {
+          @include extend-click();
+
+          .icon-clear {
+            font-size: $font-size-medium;
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+  }
+
+  .search-result {
+    flex: 1;
+    overflow: hidden;
   }
 }
 </style>
