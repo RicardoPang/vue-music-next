dc81b1f795c8d0b1c24a861ff8eaee6269ab1d85
pangfeng <pangjianfeng@huanai.com>
2021-05-17 18:24:32 +0800

    搜索页面Suggest组件开发（01）

--- src/views/search.vue
+++ src/views/search.vue
@@ -3,7 +3,7 @@
     <div class="search-input-wrapper">
       <search-input v-model="query"></search-input>
     </div>
-    <div class="search-content">
+    <div class="search-content" v-show="!query">
       <div class="hot-keys">
         <h1 class="title">热门搜索</h1>
         <ul>
@@ -17,6 +17,10 @@
         </ul>
       </div>
     </div>
+    <div class="search-result" v-show="query">
+      <suggest
+          :query="query"></suggest>
+    </div>
   </div>
 </template>
 
@@ -24,10 +28,14 @@
 import SearchInput from '@/components/search/search-input'
 import { ref } from 'vue'
 import { getHotKeys } from '@/service/search'
+import Suggest from '@/components/search/suggest'
 
 export default {
   name: 'search',
-  components: { SearchInput },
+  components: {
+    Suggest,
+    SearchInput
+  },
   setup () {
     const query = ref('')
     const hotKeys = ref([])
